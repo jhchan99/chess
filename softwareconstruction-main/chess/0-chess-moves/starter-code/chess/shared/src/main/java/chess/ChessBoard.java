b8a58959e975268454904bd95350c9ec40260b0f
jameschan <jhchan99@gmail.com>
2024-01-12 17:23:51 -0700

    first test for bishop

--- softwareconstruction-main/chess/0-chess-moves/starter-code/chess/shared/src/main/java/chess/ChessBoard.java
+++ softwareconstruction-main/chess/0-chess-moves/starter-code/chess/shared/src/main/java/chess/ChessBoard.java
@@ -1,16 +1,20 @@
 package chess;
 
+import java.util.Arrays;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
  * Note: You can add to this class, but you may not alter
  * signature of the existing methods.
  */
+
 public class ChessBoard {
+    private ChessPiece[][] squares = new ChessPiece[8][8];
 
     public ChessBoard() {
-        
-    }
+
+   }
 
     /**
      * Adds a chess piece to the chessboard
@@ -19,7 +23,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+       squares[position.getRow()][position.getColumn()] = piece;
     }
 
     /**
@@ -30,7 +34,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return squares[position.getRow()][position.getColumn()];
     }
 
     /**
--- softwareconstruction-main/instruction/java-fundamentals/example-code/03-strings/SpecialCharacterExamples.java
+++ softwareconstruction-main/instruction/java-fundamentals/example-code/03-strings/SpecialCharacterExamples.java
@@ -1,5 +1,4 @@
 public class SpecialCharacterExamples {
-
     public static void main(String [] args) {
         String stringWithNewline = "Hello\nBYU";
         System.out.println(stringWithNewline);
